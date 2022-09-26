81bf165a3611dbb15341bb183080d6367c869987
Mosunov Anton <antonmossunov@gmail.com>
2022-09-26 12:33:50 +0600

    Создание страницы для добавления новой категории
    Создана страница category_add.html для для добавления новой категории
    Добавлена маршрутизация и создано представление.
    Добавлен файл миграции.

--- allmarket/market/urls.py
+++ allmarket/market/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from market.views.tasks import index_view, product_view
+from market.views.tasks import index_view, product_view, add_category_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name='index'),
     # path('tasks/confirmation/<int:pk>', confirmation_view, name='delete_confirmation'),
-    # path('tasks/add/', add_task_view, name='task_add'),
+    path('categories/add/', add_category_view, name='category_add'),
     path('products/<int:pk>', product_view, name='product_detail'),
     # path('edit/', task_view, name='task_edit'),
     # path('tasks/edit/<int:pk>', task_edit_view, name='task_edit'),
--- allmarket/market/views/products.py
+++ allmarket/market/views/products.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from market.models import Category, Product
-
+from market.models import categories
 
 def index_view(request):
     if request.method == 'GET':
@@ -12,6 +12,26 @@ def index_view(request):
         }
         return render(request, 'index.html', context)
 
+
 def product_view(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'task.html', context={'product': product})
+
+
+def add_category_view(request):
+    if request.method == 'GET':
+        context = {
+            'categories': categories
+        }
+        return render(request, 'add_task.html', context)
+    return added_category_prepare(request)
+
+
+def added_category_prepare(request):
+    category = Category.objects.create(
+        category_name=request.POST.get("category_name"),
+        category_description=request.POST.get("category_description")
+    )
+    categories.append(category.category_name)
+    print(f'Добавлена новая категория: {category.category_name}')
+    return redirect('index')
