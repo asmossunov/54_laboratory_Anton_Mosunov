42efa70099ac08689881f9db643d030b1ed0f48d
Mosunov Anton <antonmossunov@gmail.com>
2022-09-27 17:26:05 +0600

    Редактирование представлений.
    Произведена настройка представлений для корректного отображения всех страниц

--- allmarket/market/views/products.py
+++ allmarket/market/views/products.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
-from market.models import Category, Product
+from market.models import Category, Product, StateChoices
+
 
 
 def index_view(request):
@@ -14,8 +15,16 @@ def index_view(request):
 
 
 def product_view(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, 'product.html', context={'product': product})
+    if request.method == 'GET':
+        product = get_object_or_404(Product, pk=pk)
+        return render(request, 'product.html', context={'product': product})
+
+
+def delete_product(request, pk):
+    if pk:
+        product = Product.objects.get(pk=pk)
+        product.delete()
+        return redirect('index')
 
 
 def add_category_view(request):
@@ -30,7 +39,6 @@ def added_category_prepare(request):
         category_name=request.POST.get("category_name"),
         category_description=request.POST.get("category_description")
     )
-    print(f'Добавлена новая категория: {category.category_name}')
     return redirect('index')
 
 
@@ -54,5 +62,55 @@ def added_product_prepare(request):
         price=request.POST.get("price"),
         product_image=request.POST.get("product_image")
     )
-    print(f'Добавлен новый продукт: {product.product_name}')
     return redirect(reverse('product_detail', kwargs={'pk': product.pk}))
+
+
+def categories_view(request):
+    if request.method == 'GET':
+        categories = Category.objects.filter(state='ACTIVE')
+        context = {
+            'categories': categories,
+        }
+        return render(request, 'categories.html', context)
+
+
+def delete_category(request, pk):
+    categories = Category.objects.filter(pk=pk).update(
+        state=StateChoices.NOT_ACTIVE
+    )
+    return redirect('categories')
+
+
+def category_edit_view(request, pk):
+    category = Category.objects.get(pk=pk)
+    if request.method == 'GET':
+        context = {
+            'category': category,
+        }
+        return render(request, 'edit_category.html', context)
+    category = Category.objects.filter(pk=pk).update(
+        category_name=request.POST.get("category_name"),
+        category_description=request.POST.get("category_description"),
+    )
+    return redirect('categories')
+
+
+def product_edit_view(request, pk):
+    categories = Category.objects.all()
+    product = Product.objects.get(pk=pk)
+    if request.method == 'GET':
+        context = {
+            'product': product,
+            'categories': categories,
+        }
+        return render(request, 'product_edit.html', context)
+    post_category = request.POST.get("category")
+    category = Category.objects.get(category_name=post_category)
+    product = Product.objects.filter(pk=pk).update(
+        product_name=request.POST.get("product_name"),
+        product_description=request.POST.get("product_description"),
+        category=category,
+        price=request.POST.get("price"),
+        product_image=request.POST.get("product_image")
+    )
+    return redirect('product_detail', pk)
